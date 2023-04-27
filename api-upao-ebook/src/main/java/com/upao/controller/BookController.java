ac1aba4b8c2de6a8e327943bc019d7bb4ecf9491
Abenitesl2 <113810203+Abenitesl2@users.noreply.github.com>
2023-04-27 15:46:12 -0500

    implementacion de los endpoint /api/v1/books para realiza r el POST y GET

--- api-upao-ebook/src/main/java/com/upao/controller/BookController.java
+++ api-upao-ebook/src/main/java/com/upao/controller/BookController.java
@@ -1,4 +1,39 @@
 package com.upao.controller;
 
+import com.upao.domain.Book;
+import com.upao.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
+
+    private  final BookService bookService;
+
+
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+
+    }
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks(){
+        return new ResponseEntity<List<Book>>(bookService.getAllBooks(),HttpStatus.OK);
+        
+        //Optional: return ResponseEntity.ok(bookService.getAllBooks())
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+
+        return new ResponseEntity<Book>(bookService.createBook(book),HttpStatus.CREATED);
+
+
+    }
+
+
 }
--- api-upao-ebook/src/main/java/com/upao/service/BookServiceImpl.java
+++ api-upao-ebook/src/main/java/com/upao/service/BookServiceImpl.java
@@ -1,19 +1,25 @@
 package com.upao.service;
 
 import com.upao.domain.Book;
+import com.upao.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
     @Override
     public Book createBook(Book book) {
-        return null;
+        return bookRepository.save(book);
     }
 
     @Override
     public List<Book> getAllBooks() {
-        return null;
+        return bookRepository.findAll();
     }
 }
